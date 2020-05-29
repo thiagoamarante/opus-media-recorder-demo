4cbc5b25ac2c28f09b5f6bae2f661e1984fd6332
Thiago Amarante <amarante.thiago@gmail.com>
2020-05-29 20:00:25 -0300

    Update encoderWorker.js

--- dist/encoderWorker.js
+++ dist/encoderWorker.js
@@ -3903,10 +3903,10 @@ function initWorker(workerGlobalScope) {
                 // This bug is found in Chrome 66.0.3359.181 (2018).
                 // It is fixed since 2019.
                 // So re-create Float32Array right after a web worker received it.
-                for (let i = 0; i < channelBuffers.length; i++) {
-                    channelBuffers[i] = new Float32Array(channelBuffers[i].buffer);
-                }
-                console.log("aqui1");
+                //for (let i = 0; i < channelBuffers.length; i++) {
+                //    channelBuffers[i] = new Float32Array(channelBuffers[i].buffer);
+                //}
+                console.log("aqui2");
                 //encoder.encode(channelBuffers);
                 break;
 
